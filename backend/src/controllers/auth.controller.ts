f6b831f83890e7fbd90dcd9410bc13020c1e9e85
HS <gudtn2238@gmail.com>
2025-06-23 10:53:16 +0900

    feat: [#7] 이메일 인증
    - 토큰 검사 기능 구현 중

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,14 +1,25 @@
 import { Request, Response } from 'express';
 import { registerUser } from '../services/auth.service';
+import { redisClient } from '../utils/redis';
 
 export const register = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
     await registerUser(email, password);
-    return res
-      .status(201)
-      .json({ message: '회원가입 성공. 이메일을 확인해주세요.' });
+    res.status(201).json({ message: '회원가입 성공. 이메일을 확인해주세요.' });
   } catch (err: any) {
-    return res.status(400).json({ message: err.message });
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export const verifyEmail = async (req: Request, res: Response) => {
+  const { token } = req.query;
+
+  if (!token || typeof token !== 'string') {
+    res.status(400).json({ message: '유효하지 않은 요청입니다.' });
+  }
+
+  const email = await redisClient.get(`email:verify:${token}`);
+  if (!email) {
   }
 };
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { register } from 'module';
+import { register } from '../controllers/auth.controller';
 
 const router = Router();
 
 router.post('/register', register);
+// router.get('/verify', verifyEmail);
 
 export default router;
