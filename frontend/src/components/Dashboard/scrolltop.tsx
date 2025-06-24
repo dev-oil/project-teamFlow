38d2a50a64ba14fc89aea9984bfe46e4875383f9
mkhong <mkhong210@gmail.com>
2025-06-24 10:13:21 +0900

    feat: [#8] 작업보드 ScrollTopBtn 기능 구현

--- frontend/src/components/Dashboard/scrolltop.tsx
+++ frontend/src/components/Dashboard/scrolltop.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { Button } from '../ui/button';
 import { ChevronUp } from 'lucide-react';
 
@@ -9,30 +9,37 @@ type ScrollTopButtonProps = {
 };
 
 export function ScrollTopBtn({ targetRef }: ScrollTopButtonProps) {
-  const [visible, setVisible] = useState(false);
+  // const [visible, setVisible] = useState(false);
 
-  const scrolltoTop = () => {
-    targetRef.current?.scrollTo({ top: 70, behavior: 'smooth' });
-  };
+  // useEffect(() => {
+  //   const el = targetRef.current;
+  //   console.log('ScrollTopBtn targetRef.current:', el); // 여기에 진짜 Viewport div 나와야 함
+  //   if (!el) return;
 
-  useEffect(() => {
-    const el = targetRef.current;
-    console.log('ScrollTopBtn targetRef.current:', el); // 여기에 진짜 Viewport div 나와야 함
-    if (!el) return;
+  //   const handleScroll = () => {
+  //     console.log('scrollTop:', el.scrollTop);
+  //     setVisible(el.scrollTop > 10);
+  //   };
 
-    const handleScroll = () => {
-      console.log('scrollTop:', el.scrollTop);
-      setVisible(el.scrollTop > 50);
-    };
-
-    el.addEventListener('scroll', handleScroll);
-    return () => el.removeEventListener('scroll', handleScroll);
-  }, [targetRef]);
+  //   el.addEventListener('scroll', handleScroll);
+  //   return () => el.removeEventListener('scroll', handleScroll);
+  // }, [targetRef]);
 
   // if (!visible) return null;
 
-  //이슈) 이유는 모르겠지만 ref는 viewport가 잘 나오는 있음
-  // but scroll은 인식이 안되고 버튼을 클릭했을때 그냥 스크롤바만 없어짐(다시 스크롤하면 생김)
+  //이슈) 버튼을 클릭했을때 스크롤바가 없어짐(다시 스크롤하면 생김)
+  const scrolltoTop = () => {
+    if (targetRef.current) {
+      targetRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      // targetRef.current.scrollTo({
+      //   behavior: 'smooth',
+      //   top: 0,
+      // });
+    }
+  };
 
   return (
     <Button
