5f43e8d35f71671c34bbe11669f7d1b2a0fcf31d
jjoaa <jjoaa7@gmail.com>
2025-06-26 19:20:52 +0900

    remove unused files

--- backend/src/routes/userRoutes.ts
+++
@@ -1,7 +0,0 @@
-// backend/src/routes/userRoutes.ts
-import { Router } from 'express';
-import { checkUserEmail } from '../controllers/userController'; 
-
-const router = Router();
-router.post('/check-email', checkUserEmail);
-export default router;
