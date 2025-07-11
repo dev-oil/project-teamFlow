079f6602f11b3c0e235c95bac94819a36e2cdc5a
jjoaa <jjoaa7@gmail.com>
2025-07-11 16:32:54 +0900

    fix: [#21] 무한로딩 fix

--- frontend/src/hooks/useProfileInit.ts
+++ frontend/src/hooks/useProfileInit.ts
@@ -1,16 +1,29 @@
-// src/hooks/useAuthInitializer.ts
 import { useEffect } from 'react';
 
 import { fetchUserProfile } from '@/api/user';
 import { useUserStore } from '@/stores/useUserStore';
 
+import { useAuth } from './useAuth';
+
 export function useProfileInit() {
+  const { isLoggedIn, isInitiailized } = useAuth();
+
   useEffect(() => {
     const tryGetProfile = async () => {
-      const res = await fetchUserProfile();
-      useUserStore.getState().setProfile(res);
+      try {
+        // 로그인 완료된 경우에만 profile 요청
+        if (isLoggedIn) {
+          const res = await fetchUserProfile();
+          useUserStore.getState().setProfile(res);
+        }
+      } catch (e) {
+        console.error('프로필 불러오기 실패', e);
+      }
     };
 
-    tryGetProfile();
-  }, []);
+    // 초기화 되기 전엔 아무 것도 하지 않기
+    if (isInitiailized) {
+      tryGetProfile();
+    }
+  }, [isInitiailized, isLoggedIn]);
 }
